8868e316fe163f361c9f870bba3d453575df2fdb
simja <simja@example.com>
2023-06-10 19:03:32 -0600

    guardado

--- src/main/java/modelo/Modelo.java
+++ src/main/java/modelo/Modelo.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.backend.calculadoramvc.modelo;
+package modelo;
+
+import org.nfunk.jep.JEP;
 
 /**
  *
@@ -21,10 +23,19 @@ public class Modelo {
     
     
     
-    public float Calcular(){
+    public float calcular(){
         
-        int indiceP;
         System.out.println(this.operacion);
+        Jep jep = new Jep();
+        try {
+        /*jep.addVariable("x", 10);
+        jep.parse("x+1");
+        Object result = jep.evaluate();
+    			
+        System.out.println("x + 1 = " + result);*/
+     } catch (JepException e) {
+        System.out.println("An error occurred: " + e.getMessage());
+    }
         
         return 0.0f;
     }
